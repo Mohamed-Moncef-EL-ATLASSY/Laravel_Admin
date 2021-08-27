b32a352b48dee9413e56cbc8bb5a7abf4181900b
Mohamed Moncef EL ATLASSY <mohamed.moncef.elatlassy@gmail.com>
2021-08-27 14:37:26 +0100

    goes through clients names instead of client id

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -2,11 +2,14 @@
 
 namespace App\Admin\Controllers;
 
+use App\Models\Client;
 use App\Models\Product;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
+use Hamcrest\SelfDescribing;
+use phpDocumentor\Reflection\Types\Self_;
 
 class ProductController extends AdminController
 {
@@ -28,15 +31,20 @@ class ProductController extends AdminController
 
         $grid->column('id', __('ID Produit'));
         $grid->column('productName', __('Nom Produit'));
-        // $grid->column('client.name', __('Nom Client'))->copyable()->filter("like");
-        $grid->column('client.name', __('Nom Client'))->copyable()->filter("like");
+        $grid->column('client_id', __('ID Client'))
+        ->sortable();
 
+        $grid->column('client.name', __('Nom Client'));
 
-        // $grid->column('client_id', __('ID Client'));
         $grid->column('productDescription', __('Description Produit'));
-        $grid->column('productPrice', __('Prix Produit'))->filter("range");
-        $grid->column('created_at', __('Crée le'))->hide();
-        $grid->column('updated_at', __('Modifié le'))->hide();
+        $grid->column('productPrice', __('Prix Produit'))
+        ->filter("range");
+
+        $grid->column('created_at', __('Crée le'))
+        ->hide();
+
+        $grid->column('updated_at', __('Modifié le'))
+        ->hide();
 
         return $grid;
     }
@@ -71,10 +79,22 @@ class ProductController extends AdminController
     {
         $form = new Form(new Product());
 
-        $form->text('productName', __('ProductName'));
-        $form->number('client_id', __('Client id'));
-        $form->text('productDescription', __('ProductDescription'));
-        $form->number('productPrice', __('ProductPrice'));
+        $form->select('client_id', __('Nom Client'))
+
+        ->options(Client::all()->pluck('name','id'))
+        ->required();
+
+        $form->divider();
+
+        $form->text('productName', __('Nom Produit'))
+        ->required();
+
+        $form->textarea('productDescription', __('Description Produit'))
+        ->rows(3);
+
+        $form->currency('productPrice', __('Prix Produit'))
+        ->required()
+        ->symbol("DH");
 
         return $form;
     }
